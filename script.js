68c21383a5ac8f68b9affc46edbf7b181e44748c
ajlic24@gmail.com <ajlic24@gmail.com>
2021-11-19 15:38:06 -0500

    added more js

--- script.js
+++ script.js
@@ -2,3 +2,14 @@ const h2 = document.createElement('h2');
 h2.textContent = `This content added by Javascript`;
 
 document.querySelector(`body`).appendChild(h2);
+
+const clickMe = document.getElementById(`clickMe`);
+const body = document.getElementsByTagName(`body`);
+
+clickMe.addEventListener(`click`, function(){
+    clickMe.style.textAlign = `center`;
+    h2.style.background = `black`;
+})
+
+
+
